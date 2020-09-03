a4cb6f40d87d0a2b907ce751476f993939c70619
Sayuri Hioki <sayuri.hioki12@gmail.com>
2020-09-02 22:42:17 -0300

    exercício da aula do dia 20/08

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "Olá mundo!";
+
+$saldo = 34.5; //operador de atribuição
+
+//aspas duplas aceitam string especial
+echo "<br><br><br>Seu saldo é $saldo ";
+
+$saldo = $saldo - 10; //operador de subtração
+echo "<br><br><br>Agora seu saldo é $saldo ";
+
+$saldo -= 10; //outro modo do operador de subtração
+echo "<br><br><br>Agora seu saldo é $saldo ";
+
+$saldo += 10; //operador de adição
+echo "<br><br><br>Agora seu saldo é $saldo ";
+
+$saldo *= 1000; //operador de multiplicação
+echo "<br><br><br>Agora seu saldo é $saldo ";
